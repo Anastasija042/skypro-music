5f23693b5926e5538f89e4da7c1d72f6217d30bf
Anastasija042 <anastasija_042@mail.ru>
2023-10-23 19:11:51 +0200

    com

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        
+        <p class="color_te">
+        Здесь скоро будет Skypro.Music
         </p>
         <a
           className="App-link"
