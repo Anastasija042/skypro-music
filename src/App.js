c7298831d7c722640fbc0caaf69a0538cbf3b8cf
Anastasija042 <anastasija_042@mail.ru>
2023-10-23 19:15:34 +0200

    hw1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="App-header">        
         <p class="color_te">
         Здесь скоро будет Skypro.Music
         </p>
